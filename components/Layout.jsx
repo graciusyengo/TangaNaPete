7befaabd816fb23b51ca9fd03c3ff6cc27f46636
gracius yengo <yengogracius@gmail.com>
2023-04-07 01:45:58 +0200

    refactor: delete space in the components Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,6 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import styles from "../styles/Layout.module.css"
-
 export default function Layout ({children}){
     return(
         <div className={styles.container}>
