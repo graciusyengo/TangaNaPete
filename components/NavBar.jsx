449cc2904e87133c72013957e8daa2ef9b3b909c
gracius yengo <yengogracius@gmail.com>
2023-04-07 17:58:35 +0200

    feat:add style in the component navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,28 @@
-import styles from "../styles/NavBar.module.css"
-export default function NavBar(){
-    return(
-        <div className={styles.container}>NAVBAR</div>
-    )
+import styles from "../styles/NavBar.module.css";
+export default function NavBar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.containerLogo}>
+          <h2 className={styles.logo}>TangaNaPete</h2>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Epreuve</li>
+          <li className={styles.listItem}>Nous Contactez</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.auth}>
+          <div className={styles.containerInscription}>
+            <span className={styles.inscription}>S'inscrire</span>
+          </div>
+          <div className={styles.containerConnexion}>
+            <span className={styles.connexion}>Connexion</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
