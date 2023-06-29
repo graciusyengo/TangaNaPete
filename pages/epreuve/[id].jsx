b1413f977dd6faadf3ecbec2afdbaa9e645425ac
gracius <yengogracius@gmail.com>
2023-06-29 12:25:40 +0100

    feat : add create and doawload pdf

--- pages/epreuve/[id].jsx
+++ pages/epreuve/[id].jsx
@@ -1,17 +1,26 @@
+import React from "react";
 import styles from "../../styles/Epreuve.module.css";
 import Image from "next/image";
 import EpreuveListSimilary from "../../components/epreuveListSimilary";
 import db from "../../util/mongoDb";
 import { useRouter } from "next/router";
 import axios from "axios";
+import  Pdf from "react-to-pdf"
 
+const ref=React.createRef()
+console.log(ref)
 export default function Epreuve({ epreuve }) {
   console.log(epreuve);
+  const options = {
+    orientation: 'landscape',
+    unit: 'in',
+    format: [6,6]
+}; 
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.item}>
+        <div ref={ref} className={styles.item}>
           <Image
             src={epreuve.img}
             height={100}
@@ -39,8 +48,11 @@ export default function Epreuve({ epreuve }) {
             </div>
           </div>
           <div className={styles.buttonContainer}>
-            <button className={styles.button}> Telecharger</button>
-            <button className={styles.button}> Voir</button>
+           
+
+         <Pdf targetRef={ref} fillname="epreuve_doawload.pdf" options={options} x={.5}>
+          {({toPdf})=> <button className={styles.button} onClick={toPdf}> Telecharger</button>}
+           </Pdf>
           </div>
         </div>
       </div>
