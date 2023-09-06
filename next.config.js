717687d0a470c08f5fa07477b5313a7dc751496f
gracius yengo <yengogracius@gmail.com>
2023-09-06 21:54:52 +0200

    correction bug next/image in file next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
 }
-module.exports = nextConfig
+
